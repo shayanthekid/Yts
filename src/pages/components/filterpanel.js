5186ac1cedbcb48ea70c9103a04a6b2743f00cb6
shayanthekid <sajidfayazhaniff@gmail.com>
2024-01-16 12:44:21 +0530

    added the date picker yay

--- src/pages/components/filterpanel.js
+++ src/pages/components/filterpanel.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Calendar from "@demark-pro/react-booking-calendar";
 
 const FilterPanel = () => {
+    const [isDatePickerOpen, setIsDatePickerOpen] = useState(false);
+    const [selectedDates, setSelectedDates] = useState([]);
+
+    const handleAvailabilityClick = () => {
+        setIsDatePickerOpen(!isDatePickerOpen);
+    };
+
+    const handleChange = (dates) => {
+        setSelectedDates(dates);
+    };
+
     return (
         <div className="bg-gray-100 p-4 rounded-md shadow-md">
             {/* Filter Panel Content */}
             {/* Include Search bar, Date picker, Price range picker, and More button */}
-           
 
             {/* Search Bar */}
             <div className="flex items-center mb-4">
@@ -27,7 +38,7 @@ const FilterPanel = () => {
             {/* Date Picker, Price Range Picker, More Button */}
             <div className="flex items-center">
                 {/* Date Picker */}
-                <div className="flex items-center mr-4">
+                <div className="flex items-center mr-4" onClick={handleAvailabilityClick}>
                     <p className="mr-2">Availability</p>
                     <span className="text-gray-500">
                         {/* You can replace the following line with your desired icon */}
@@ -39,30 +50,31 @@ const FilterPanel = () => {
 
                 {/* Price Range Picker */}
                 <div className="flex items-center mr-4">
-                    <p className="mr-2">Price Range</p>
-                    <span className="text-gray-500">
-                        {/* You can replace the following line with your desired icon */}
-                        <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            {/* Icon code goes here */}
-                        </svg>
-                    </span>
+                    {/* ... (your existing price range picker code) */}
                 </div>
 
                 {/* More Button */}
                 <button className="flex items-center">
-                    <span className="mr-2">More</span>
-                    <span className="text-gray-500">
-                        {/* You can replace the following line with your desired icon */}
-                        <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            {/* Icon code goes here */}
-                        </svg>
-                    </span>
+                    {/* ... (your existing more button code) */}
                 </button>
             </div>
-            
-        </div>
 
+            {/* Show Date Picker if Availability is clicked */}
+            {isDatePickerOpen && (
+                <div className="mt-4">
+                    <Calendar
+                        selected={selectedDates}
+                        onChange={handleChange}
+                        onOverbook={(e, err) => alert(err)}
+                        disabled={(date, state) => !state.isSameMonth}
+                        range={true}
+                        dateFnsOptions={{ weekStartsOn: 1 }}
+                    />
+                </div>
+            )}
+        </div>
     );
 };
+
 
 export default FilterPanel;
