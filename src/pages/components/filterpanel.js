fbdc56fd3b7e55d7f6ec3bc9b97ee8b95a014c81
shayanthekid <sajidfayazhaniff@gmail.com>
2024-01-17 17:24:35 +0530

    fixed minor errors

--- src/pages/components/filterpanel.js
+++ src/pages/components/filterpanel.js
@@ -41,7 +41,7 @@ const FilterPanel = ({ type }) => {
 
 {isDesktop ? (
                 //Desktop
-                <div className="flex absolute  h-screen  -mt-32">
+                <div className="flex absolute  h-screen  -mt-72">
                     <div className="bg-gray-100 p-4 rounded-xl shadow-md w-1/2 ml-32 ">
                         {/* Search Bar */}
                         <div className="relative flex items-center">
--- src/pages/components/footer.js
+++ src/pages/components/footer.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import bookingimg from '../../assets/images/ytslogo.png';
 
 function Footer() {
-    return (
-        <div className="bg-gray-200 p-4 text-center">
-            {/* Navigation Heading */}
-          
+    const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 768);
+    useEffect(() => {
+        const handleResize = () => {
+            setIsDesktop(window.innerWidth >= 768);
+        };
 
-            {/* Logo */}
-            <img src={bookingimg} alt="Logo" className="w-16 h-16 mx-auto mb-4" />
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+    return (
+
+<>
+  {isDesktop ? (
+                // Desktop
+              
+                    <div className="bg-gray-200 p-4 text-center relative bottom-0 mt-96">
+
+                        <img src={bookingimg} alt="Logo" className="h-8 w-auto mx-auto mb-4" />
+                        <h2 className="text-xl font-medium mb-4 text-left">Navigation</h2>
+                        {/* Links */}
+                        <div className="flex flex-col items-start">
+                            <Link to="/home" className="text-black py-2 px-4 font-light">
+                                Home
+                            </Link>
+                            <Link to="/listingcar" className="text-black py-2 px-4 font-light">
+                                Cars
+                            </Link>
+                            <Link to="/listingproperty" className="text-black py-2 px-4 font-light">
+                                Property
+                            </Link>
+                            <Link to="/listingvacation" className="text-black py-2 px-4 font-light">
+                                Vacation Rentals
+                            </Link>
+                        </div>
+                    </div >
+               
+            ) : (
+                // Mobile
+                    <div className = "bg-gray-200 p-4 text-center">
+            
+            <img src = { bookingimg } alt = "Logo" className = "h-8 w-auto mx-auto mb-4" />
             <h2 className="text-xl font-medium mb-4 text-left">Navigation</h2>
             {/* Links */}
             <div className="flex flex-col items-start">
@@ -26,7 +63,11 @@ function Footer() {
                     Vacation Rentals
                 </Link>
             </div>
-        </div>
+        </div >
+            )}
+</>
+
+       
     );
 }
 
