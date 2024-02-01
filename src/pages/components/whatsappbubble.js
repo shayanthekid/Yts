5631648600842e1a5c01fd1cef56723812dd7b84
shayanthekid <sajidfayazhaniff@gmail.com>
2024-02-01 12:51:01 +0530

    again 5

--- src/pages/components/whatsappbubble.js
+++ src/pages/components/whatsappbubble.js
@@ -3,9 +3,9 @@ import whatsappicon from '../../assets/images/featureicons/whatsapp.svg';
 
 const WhatsAppBubble = () => {
     return (
-        <div className="fixed bottom-8 right-8 z-50 transition-transform bg-[#075E54] p-3 rounded-full transform hover:scale-110">
+        <div className="fixed bottom-8 right-2 z-50 transition-transform bg-[#075E54] p-2 rounded-full transform hover:scale-110">
             <a aria-label="Chat on WhatsApp" href="https://wa.me/94742081318" target="_blank" rel="noopener noreferrer">
-                <img alt="Chat on WhatsApp" src={whatsappicon} className="w-10 h-auto cursor-pointer" />
+                <img alt="Chat on WhatsApp" src={whatsappicon} className="w-8 h-auto cursor-pointer" />
             </a>
         </div>
     );
